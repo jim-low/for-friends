fe4f78d7b7d949a5596379468d78fe0af4fcfae4
jim-low <robil12380@gmail.com>
2020-09-23 20:15:41 +0800

    added functionalities to display message and extra styling

--- claudia.js
+++ claudia.js
@@ -1,28 +1,29 @@
+let form = document.querySelector("#form")
+let msg = document.querySelector("#message")
 let name = document.querySelector(".name input")
 let moodLevel = document.querySelector(".mood input")
 let msgType = document.querySelector(".message-type select")
 let submitBtn = document.querySelector(".submit-btn")
 let resetBtn = document.querySelector(".reset-btn")
 let cssEmptyInput = "background: rgba(241, 144, 144, 0.7); border-color: rgb(235, 75, 75);"
-let cssNameInput = "background: none; border-bottom: 2px solid rgb(118, 118, 118);"
-
+name.value = "Jim"
 const emptyName = () => {
 	return name.value === ""
 }
 
 let messages = 
 {
-	comfort: {
+	comforting: {
 		good: "comforting good",
 		bad: "comforting bad",
 		neutral: "comforting neutral"
 	},
-	motivate: {
+	motivating: {
 		good: "motivating good",
 		bad: "motivating bad",
 		neutral: "motivating neutral"
 	},
-	encourage: {
+	encouraging: {
 		good: "encouraging good",
 		bad: "encouraging bad",
 		neutral: "encouraging neutral"
@@ -31,13 +32,35 @@ let messages =
 
 let surpriseMessage = "this is surprise"
 
+const renderMessage = () => {
+	let level = Number(moodLevel.value)
+	let typeMsg = messages[msgType.value]
+	let msgContainer = document.querySelector(".msg")
+	if(msgType.value !== "birthday"){
+		if(level <= 2){
+			msgContainer.innerHTML = typeMsg.bad
+		}else if (level === 3){
+			msgContainer.innerHTML = typeMsg.neutral
+		}else {
+			msgContainer.innerHTML = typeMsg.good
+		}
+	}else {
+		msgContainer.innerHTML = surpriseMessage
+	}
+	$("#form").fadeOut(3000);
+	$("#message").fadeIn(3000);
+}
+
 submitBtn.addEventListener("mousedown", () => {
 	if(emptyName()){
 		name.placeholder = "Please fill in this field."
 		name.style.cssText = cssEmptyInput
 	}else {
-		name.placeholder = "Your Name"
-		name.style.cssText = cssNameInput
+		if(confirm("Confirm options")){
+			renderMessage()
+			form.style.display = "none"
+			msg.style.display = "flex"
+		}
 	}
 })
 
