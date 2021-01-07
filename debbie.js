459bab63a50f62008b869c1dd88fc8bb1ab8a8dd
jim-low <robil12380@gmail.com>
2021-01-07 16:36:42 +0800

    completed debbie's project

--- debbie.js
+++ debbie.js
@@ -1,2 +1,23 @@
+const cardFront = document.querySelector('.front')
+const innerPages = document.querySelectorAll('.inner .page')
 
+cardFront.addEventListener('click', () => {
+    cardFront.style.animation = "moveRight .5s ease-out forwards"
+    setTimeout(() => {
+        cardFront.style.display = "none"
+        innerPages[0].parentNode.style.display = "flex"
+        innerPages[0].style.animation = "moveLeft .5s ease-out forwards"
+    }, 510)
+})
+
+innerPages.forEach(page => {
+    page.addEventListener('click', () => {
+        innerPages[0].style.animation = "moveRight .5s ease-out forwards"
+        setTimeout(() => {
+            cardFront.style.display = "flex"
+            cardFront.style.animation = "moveLeft .5s ease-out forwards"
+            innerPages[0].parentNode.style.display = "none"
+        }, 600)
+    })
+})
 
