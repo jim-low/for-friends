37f30087210790e7cb99ad28cc50cde37241f22c
jim-low <robil12380@gmail.com>
2021-01-07 18:19:59 +0800

    finished base, responsiveness is next

--- debbie.js
+++ debbie.js
@@ -1,23 +1,47 @@
 const cardFront = document.querySelector('.front')
 const innerPages = document.querySelectorAll('.inner .page')
+let phoneMedia = window.matchMedia("(max-width: 480px)")
 
-cardFront.addEventListener('click', () => {
-    cardFront.style.animation = "moveRight .5s ease-out forwards"
-    setTimeout(() => {
-        cardFront.style.display = "none"
-        innerPages[0].parentNode.style.display = "flex"
-        innerPages[0].style.animation = "moveLeft .5s ease-out forwards"
-    }, 510)
-})
-
-innerPages.forEach(page => {
-    page.addEventListener('click', () => {
-        innerPages[0].style.animation = "moveRight .5s ease-out forwards"
+if(phoneMedia.matches) {
+    cardFront.addEventListener('click', () => {
+        cardFront.style.animation = "moveDown .5s ease-out forwards"
         setTimeout(() => {
-            cardFront.style.display = "flex"
-            cardFront.style.animation = "moveLeft .5s ease-out forwards"
-            innerPages[0].parentNode.style.display = "none"
+            cardFront.style.display = "none"
+            innerPages[0].parentNode.style.display = "flex"
+            innerPages[0].style.animation = "moveUp .5s ease-out forwards"
         }, 600)
     })
-})
+
+    innerPages.forEach(page => {
+        page.addEventListener('click', () => {
+            innerPages[0].style.animation = "moveDown .5s ease-out forwards"
+            setTimeout(() => {
+                cardFront.style.display = "flex"
+                cardFront.style.animation = "moveUp .5s ease-out forwards"
+                innerPages[0].parentNode.style.display = "none"
+            }, 600)
+        })
+    })
+}else {
+    cardFront.addEventListener('click', () => {
+        cardFront.style.animation = "moveRight .5s ease-out forwards"
+        setTimeout(() => {
+            cardFront.style.display = "none"
+            innerPages[0].parentNode.style.display = "flex"
+            innerPages[0].style.animation = "moveLeft .5s ease-out forwards"
+        }, 600)
+    })
+
+    innerPages.forEach(page => {
+        page.addEventListener('click', () => {
+            innerPages[0].style.animation = "moveRight .5s ease-out forwards"
+            setTimeout(() => {
+                cardFront.style.display = "flex"
+                cardFront.style.animation = "moveLeft .5s ease-out forwards"
+                innerPages[0].parentNode.style.display = "none"
+            }, 600)
+        })
+    })
+}
+
 
