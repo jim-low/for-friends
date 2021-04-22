b48c44bd4445ea51ef4a9ff668f3fb0c168663b9
jim-low <robil12380@gmail.com>
2021-04-22 16:22:47 +0800

    changed parameter name to being more meaningful

--- yinShing2.js
+++ yinShing2.js
@@ -1,14 +1,14 @@
 const birthdayWords = document.querySelectorAll('.birthday-word');
 
-function setAnimations(initDur) {
+function setAnimations(enlargeDuration) {
     let subDur = 0;
 
     for (let i = 0; i < birthdayWords.length; ++i) {
         setTimeout(() => {
-            birthdayWords[i].style.animation = `enlarge ${initDur}s cubic-bezier(0.5, 0.6, 0.7, 0.65) forwards, glow 1s ease-in infinite`;
+            birthdayWords[i].style.animation = `enlarge ${enlargeDuration}s cubic-bezier(0.5, 0.6, 0.7, 0.65) forwards, glow 1s ease-in infinite`;
         }, (subDur - i) * 1000);
 
-        subDur += initDur;
+        subDur += enlargeDuration;
     }
 }
 
