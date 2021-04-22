c6d01ac2adfbcface7723db4e00d59d8724fc40c
jim-low <robil12380@gmail.com>
2021-04-22 15:56:18 +0800

    fixed enlarge and glow animations on each element

--- yinShing2.js
+++ yinShing2.js
@@ -5,8 +5,8 @@ function setAnimations(initDur) {
 
     for (let i = 0; i < birthdayWords.length; ++i) {
         setTimeout(() => {
-            birthdayWords[i].style.animation = `enlarge ${initDur}s cubic-bezier(0.5, 0.6, 0.7, 0.65) forwards`;
-        }, subDur * 1000);
+            birthdayWords[i].style.animation = `enlarge ${initDur}s cubic-bezier(0.5, 0.6, 0.7, 0.65) forwards, glow 1s ease-in infinite`;
+        }, (subDur - i) * 1000);
 
         subDur += initDur;
     }
