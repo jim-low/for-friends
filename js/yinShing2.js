9c54bd79786d5b526a538ee4b53729b08342fe22
jim-low <robil12380@gmail.com>
2021-04-23 17:06:22 +0800

    finished basic code to get balls working

--- js/yinShing2.js
+++ js/yinShing2.js
@@ -17,3 +17,50 @@ setTimeout(() => {
     setAnimations(2);
 }, 500);
 
+// start of balls animation code
+const canvas = document.getElementById('balls-canvas');
+const ctx = canvas.getContext('2d');
+
+const MAX_SPEED = 50;
+const MIN_SPEED = 10;
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+class Ball {
+    constructor(x, y, radius, speed) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.speed = speed;
+
+        this.dx = (Math.random() * MAX_SPEED) + MIN_SPEED;
+        this.dy = (Math.random() * MAX_SPEED) + MIN_SPEED;
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2);
+        context.fillStyle = colors[Math.floor(Math.random() * colors.length)];
+        context.fill();
+        ctx.stroke();
+    }
+
+    move() {
+        if(this.x + this.radius > canvas.width || this.x - this.radius < 0)
+            this.dx *= -1;
+
+        if(this.y + this.radius > canvas.height || this.y - this.radius < 0)
+            this.dy *= -1;
+
+        this.x += this.dx;
+        this.y += this.dy;
+    }
+
+    update() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        this.move();
+        this.draw();
+    }
+}
+
