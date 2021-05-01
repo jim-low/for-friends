1de6026b5544b6e826dbbab8e58652a59febbbf4
jim-low <robil12380@gmail.com>
2021-05-01 13:22:44 +0800

    removed commented code

--- js/yinShing2.js
+++ js/yinShing2.js
@@ -60,7 +60,6 @@ class Ball {
         ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2);
         ctx.fillStyle = this.color;
         ctx.fill();
-        // ctx.stroke();
     }
 
     bounce() {
