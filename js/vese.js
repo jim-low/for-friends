cd206460f0471a3b3707b4593c56da5138515f21
jim-low <robil12380@gmail.com>
2021-05-10 12:20:07 +0800

    added Particle class and basic funtionality, changed variable c to canvas

--- js/vese.js
+++ js/vese.js
@@ -1,8 +1,8 @@
-const c = document.getElementById('oscillation')
-const ctx = c.getContext('2d');
+const canvas = document.getElementById('oscillation')
+const ctx = canvas.getContext('2d');
 
-c.width = window.innerWidth;
-c.height = window.innerHeight;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
 const mouse = {
     x: undefined,
@@ -15,7 +15,64 @@ addEventListener('mousemove', (e) => {
 });
 
 window.addEventListener('resize', () => {
-    c.width = window.innerWidth;
-    c.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 });
+
+class Particle {
+    constructor(props) {
+        this.pos = props.pos;
+        this.axis = this.pos;
+
+        this.radius = props.radius;
+        this.color = props.color;
+
+        this.velocity = 0.05;
+        this.radian = 0;
+    }
+
+    draw() {
+        ctx.fillStyle = this.color;
+
+        ctx.beginPath();
+        ctx.arc(this.pos.x, this.pos.y, this.radius, 0, Math.PI*2);
+        ctx.closePath();
+
+        ctx.fill();
+    }
+
+    update() {
+        this.radian += this.velocity;
+
+        this.pos.x = mouse.x + Math.cos(this.radian) * 50;
+        this.pos.y = mouse.y + Math.sin(this.radian) * 50;
+    }
+}
+
+let particles = [];
+
+for(let i = 0; i < 50; ++i) {
+    let randRadius = Math.floor(Math.random() * 5);
+    particles.push(new Particle({
+        pos: {
+            x: 0,
+            y: 0,
+        },
+        radius: randRadius,
+        color: 'red'
+    }));
+}
+
+function refresh() {
+    requestAnimationFrame(refresh);
+    // ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = 'rgba(255, 255, 255, 0.02)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    particles.forEach(particle => {
+        particle.draw();
+        particle.update();
+    });
+}
+
+refresh();
 
