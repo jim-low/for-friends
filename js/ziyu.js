f90f068143f69700e510d4801d44308f7607f13b
jim-low <robil12380@gmail.com>
2021-05-23 17:45:46 +0800

    fixed indentation of file

--- js/ziyu.js
+++ js/ziyu.js
@@ -5,9 +5,9 @@ canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
 const mouse = {
-	x: canvas.width / 2,
-	y: canvas.height / 2,
-	radius: 30
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    radius: 30
 }
 const MAX_LINE_LENGTH = 150
 const MIN_LINE_LENGTH = 60
@@ -18,135 +18,135 @@ let hue = 0
 let angle = 0
 
 window.addEventListener('resize', () => {
-	canvas.width = window.innerWidth
-	canvas.height = window.innerHeight
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
 
-	setLineLength()
+    setLineLength()
 })
 
 function setLineLength() {
-	lineLength = canvas.width / 10
-	if (lineLength < MIN_LINE_LENGTH) {
-		lineLength = MIN_LINE_LENGTH
-	} else if (lineLength > MAX_LINE_LENGTH) {
-		lineLength = MAX_LINE_LENGTH
-	}
+    lineLength = canvas.width / 10
+    if (lineLength < MIN_LINE_LENGTH)
+        lineLength = MIN_LINE_LENGTH
 
-	ctx.clearRect(0, 0, canvas.width, canvas.height)
+    if (lineLength > MAX_LINE_LENGTH)
+        lineLength = MAX_LINE_LENGTH
 
-	triangles[0] = new Triangle({
-		pos: {
-			x: canvas.width / 2,
-			y: canvas.height / 2,
-		},
-		lineLength: lineLength,
-		rotateSpeed: 0.025,
-		shrinkRate: 0,
-		followMouse: true,
-	})
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+    triangles[0] = new Triangle({
+        pos: {
+            x: canvas.width / 2,
+            y: canvas.height / 2,
+        },
+        lineLength: lineLength,
+        rotateSpeed: 0.025,
+        shrinkRate: 0,
+        followMouse: true,
+    })
 }
 
 function toRadian(degree) {
-	return (degree / 180) * Math.PI
+    return (degree / 180) * Math.PI
 }
 
 class Triangle {
-	constructor(props) {
-		this.pos = props.pos
-		this.lineLength = props.lineLength
-		this.rotateSpeed = props.rotateSpeed
-		this.shrinkRate = props.shrinkRate
-		this.angle = angle
-		this.followMouse = props.followMouse || false
-	}
+    constructor(props) {
+        this.pos = props.pos
+        this.lineLength = props.lineLength
+        this.rotateSpeed = props.rotateSpeed
+        this.shrinkRate = props.shrinkRate
+        this.angle = angle
+        this.followMouse = props.followMouse || false
+    }
 
-	draw() {
-		const p1 = {
-			x: this.pos.x + Math.cos(this.angle) * this.lineLength,
-			y: this.pos.y + Math.sin(this.angle) * this.lineLength,
-		}
-		const p2 = {
-			x: this.pos.x + Math.cos(this.angle + toRadian(120)) * this.lineLength,
-			y: this.pos.y + Math.sin(this.angle + toRadian(120)) * this.lineLength,
-		}
-		const p3 = {
-			x: this.pos.x + Math.cos(this.angle + toRadian(240)) * this.lineLength,
-			y: this.pos.y + Math.sin(this.angle + toRadian(240)) * this.lineLength,
-		}
+    draw() {
+        const p1 = {
+            x: this.pos.x + Math.cos(this.angle) * this.lineLength,
+            y: this.pos.y + Math.sin(this.angle) * this.lineLength,
+        }
+        const p2 = {
+            x: this.pos.x + Math.cos(this.angle + toRadian(120)) * this.lineLength,
+            y: this.pos.y + Math.sin(this.angle + toRadian(120)) * this.lineLength,
+        }
+        const p3 = {
+            x: this.pos.x + Math.cos(this.angle + toRadian(240)) * this.lineLength,
+            y: this.pos.y + Math.sin(this.angle + toRadian(240)) * this.lineLength,
+        }
 
-		ctx.strokeStyle = `hsl(${hue}, 90%, 50%)`
-		ctx.beginPath()
-		ctx.moveTo(p1.x, p1.y)
-		ctx.lineTo(p2.x, p2.y)
-		ctx.lineTo(p3.x, p3.y)
-		ctx.lineTo(p1.x, p1.y)
-		ctx.closePath()
-		ctx.stroke()
-	}
+        ctx.strokeStyle = `hsl(${hue}, 90%, 50%)`
+        ctx.beginPath()
+        ctx.moveTo(p1.x, p1.y)
+        ctx.lineTo(p2.x, p2.y)
+        ctx.lineTo(p3.x, p3.y)
+        ctx.lineTo(p1.x, p1.y)
+        ctx.closePath()
+        ctx.stroke()
+    }
 
-	update() {
-		this.angle += this.rotateSpeed
-		this.lineLength -= this.shrinkRate
+    update() {
+        this.angle += this.rotateSpeed
+        this.lineLength -= this.shrinkRate
 
-		if (!this.followMouse)
-			return
+        if (!this.followMouse)
+            return
 
-		this.pos.x += (mouse.x - this.pos.x) * 0.05
-		this.pos.y += (mouse.y - this.pos.y) * 0.05
-	}
+        this.pos.x += (mouse.x - this.pos.x) * 0.05
+        this.pos.y += (mouse.y - this.pos.y) * 0.05
+    }
 }
 
 const triangles = []
 
 function refresh() {
-	requestAnimationFrame(refresh)
-	ctx.clearRect(0, 0, canvas.width, canvas.height)
-	triangles.forEach((triangle, i) => {
-		triangle.draw()
-		triangle.update()
-		if (triangle.lineLength <= 5)
+    requestAnimationFrame(refresh)
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    triangles.forEach((triangle, i) => {
+        triangle.draw()
+        triangle.update()
+        if (triangle.lineLength <= 5)
             triangles.splice(i, 1)
-	})
-	angle += 0.025
-	hue += 1
+    })
+    angle += 0.025
+    hue += 1
 }
 
 // initial static triangle
 triangles.push(
-	new Triangle({
-		pos: {
-			x: mouse.x,
-			y: mouse.y,
-		},
-		lineLength: lineLength,
-		rotateSpeed: ROTATE_SPEED,
-		shrinkRate: 0,
-		followMouse: true,
-	})
+    new Triangle({
+        pos: {
+            x: mouse.x,
+            y: mouse.y,
+        },
+        lineLength: lineLength,
+        rotateSpeed: ROTATE_SPEED,
+        shrinkRate: 0,
+        followMouse: true,
+    })
 )
 
 function spawnTriangle() {
-	triangles.push(
-		new Triangle({
-			pos: {
-				x: triangles[0].pos.x,
-				y: triangles[0].pos.y,
-			},
-			lineLength: lineLength,
-			rotateSpeed: ROTATE_SPEED,
-			shrinkRate: lineLength / 50,
-		})
-	)
-	if(Math.hypot((mouse.y - triangles[0].pos.y), (mouse.x - triangles[0].pos.x)) > mouse.radius)
+    triangles.push(
+        new Triangle({
+            pos: {
+                x: triangles[0].pos.x,
+                y: triangles[0].pos.y,
+            },
+            lineLength: lineLength,
+            rotateSpeed: ROTATE_SPEED,
+            shrinkRate: lineLength / 50,
+        })
+    )
+    if(Math.hypot((mouse.y - triangles[0].pos.y), (mouse.x - triangles[0].pos.x)) > mouse.radius)
         setTimeout(spawnTriangle)
 }
 
 ['mousemove', 'touchmove'].forEach((triggerEvent) => {
-	addEventListener(triggerEvent, (e) => {
-		mouse.x = e.clientX ? e.clientX : e.touches[0].pageX
-		mouse.y = e.clientY ? e.clientY : e.touches[0].pageY
-		spawnTriangle()
-	})
+    addEventListener(triggerEvent, (e) => {
+        mouse.x = e.clientX ? e.clientX : e.touches[0].pageX
+        mouse.y = e.clientY ? e.clientY : e.touches[0].pageY
+        spawnTriangle()
+    })
 })
 
 refresh()
