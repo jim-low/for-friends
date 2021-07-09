909813867a0186fd6b9503796d67368eaaea36af
jim-low <robil12380@gmail.com>
2021-07-09 22:50:21 +0800

    added project files with basic boilerplate

---
+++ js/laser-trace.js
@@ -0,0 +1,12 @@
+const canvas = document.getElementById("laser-trace");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+class Sparkle {
+}
+
+class Laser {
+}
+
