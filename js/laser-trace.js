232ffb2560fb3067166093e204d9ba52ec6d306b
jim-low <robil12380@gmail.com>
2021-07-10 16:32:47 +0800

    added code to render sparks onto screen without animation

--- js/laser-trace.js
+++ js/laser-trace.js
@@ -4,9 +4,70 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-class Sparkle {
+const mouse = {
+    x: null,
+    y: null,
+}
+
+let mouseDown = false;
+
+class SparkleEffect {
+    constructor() {
+        this.pos = {
+            x: mouse.x,
+            y: mouse.y,
+        };
+    }
+
+    draw(lastPos = this.pos) {
+        ctx.beginPath();
+
+        ctx.strokeStyle = "white";
+        ctx.lineWidth = 2;
+
+        ctx.moveTo(lastPos.x, lastPos.y);
+        ctx.lineTo(this.pos.x, this.pos.y);
+        ctx.stroke();
+
+        ctx.closePath();
+    }
+
+    update() {
+        const lastPos = {
+            x: this.pos.x,
+            y: this.pos.y,
+        };
+
+        this.pos.y += 1;
+
+        this.draw(lastPos);
+        console.log(this.pos);
+    }
 }
 
 class Laser {
 }
+
+addEventListener("mousedown", () => mouseDown = true );
+addEventListener("mouseup", () => mouseDown = false );
+addEventListener("mousemove", (e) => {
+    mouse.x = e.clientX;
+    mouse.y = e.clientY;
+});
+
+const sparks = [];
+
+function animateSparks() {
+    sparks.forEach(spark => spark.update());
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    if (mouseDown)
+        sparks.push(new SparkleEffect());
+    animateSparks();
+}
+
+animate();
 
